28ba75234658cf5c4a31f6b1f2b014922d754d63
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-28 23:33:48 -0500

    14. useEffect - CounterEffect

---
+++ src/bases/CounterEffect.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+const MAXIMUN_COUNT = 10;
+
+export const CounterEffect = () => {
+	const [counter, setCounter] = useState(5);
+
+	const handleClick = () => {
+		setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
+	};
+
+	useEffect(() => {
+		if (counter < 10) return;
+
+		console.log(
+			'%cSe llego al valor máximo',
+			'color: red; background-color: black'
+		);
+	}, [counter]);
+
+	return (
+		<>
+			<h1>CounterEffect: {counter}</h1>
+			<button onClick={handleClick}>+1</button>
+		</>
+	);
+};
