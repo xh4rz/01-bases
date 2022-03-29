c87f55ff62231f5e795cdc4841857b586d764aae
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-28 23:16:54 -0500

    13. Optimizaciones y tipado

--- src/bases/CounterBy.tsx
+++ src/bases/CounterBy.tsx
@@ -4,18 +4,21 @@ interface Props {
 	initialValue?: number;
 }
 
+interface CounterState {
+	counter: number;
+	clicks: number;
+}
+
 export const CounterBy = ({ initialValue = 5 }: Props) => {
-	const [counterState, setCounterState] = useState({
+	const [{ counter, clicks }, setCounterState] = useState<CounterState>({
 		counter: initialValue,
 		clicks: 0
 	});
 
-	const { counter, clicks } = counterState;
-
 	const handleClick = (value: number) => {
-		setCounterState((prev) => ({
-			counter: prev.counter + value,
-			clicks: prev.clicks + 1
+		setCounterState(({ clicks, counter }) => ({
+			counter: counter + value,
+			clicks: clicks + 1
 		}));
 	};
 
