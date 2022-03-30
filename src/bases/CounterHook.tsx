b8f0bcba6ee94f4454f5b4b3f1b3238e38945d23
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-29 22:48:38 -0500

    17. CustomHooks con referencias HTML

---
+++ src/bases/CounterHook.tsx
@@ -0,0 +1,13 @@
+import { useCounter } from '../hooks/useCounter';
+
+export const CounterHook = () => {
+	const { counter, counterElement, handleClick } = useCounter();
+
+	return (
+		<>
+			<h1>Counter Hook:</h1>
+			<h2 ref={counterElement}>{counter}</h2>
+			<button onClick={handleClick}>+1</button>
+		</>
+	);
+};
---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+
+const MAXIMUN_COUNT = 10;
+
+export const useCounter = () => {
+	const [counter, setCounter] = useState(5);
+	const counterElement = useRef<HTMLHeadingElement>(null);
+
+	const handleClick = () => {
+		setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
+	};
+
+	useEffect(() => {
+		if (counter < 10) return;
+
+		console.log(
+			'%cSe llego al valor máximo',
+			'color: red; background-color: black'
+		);
+
+		const tl = gsap.timeline();
+
+		tl.to(counterElement.current, {
+			y: -10,
+			duration: 0.2,
+			ease: 'ease.out'
+		}).to(counterElement.current, {
+			y: 0,
+			duration: 1,
+			ease: 'bounce.out'
+		});
+	}, [counter]);
+
+	return {
+		counter,
+		counterElement,
+		handleClick
+	};
+};
