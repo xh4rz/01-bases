98f26c9a6fb6322c2bbd3abb1183ed7fe73fafe2
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-19 13:47:36 -0500

    20. Función pura - Reducer

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -7,22 +7,48 @@ interface CounterState {
 }
 
 const INITIAL_STATE: CounterState = {
-	counter: 0,
-	previous: 0,
-	changes: 0
+	counter: 10,
+	previous: 15,
+	changes: 20
+};
+
+type CounterAction =
+	| {
+			type: 'increaseBy';
+			payload: { value: number };
+	  }
+	| {
+			type: 'reset';
+	  };
+
+const counterReducer = (
+	state: CounterState,
+	action: CounterAction
+): CounterState => {
+	switch (action.type) {
+		case 'reset':
+			return {
+				changes: 0,
+				counter: 0,
+				previous: 0
+			};
+
+		default:
+			return state;
+	}
 };
 
 export const CounterReducerComponent = () => {
-	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+	const [{ counter }, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
 	const handleClick = () => {
-		setCounter((prev) => prev + 1);
+		dispatch({ type: 'reset' });
 	};
 
 	return (
 		<>
 			<h1>Counter Reducer: {counter}</h1>
-			<button onClick={handleClick}>+1</button>
+			<button onClick={handleClick}>Reset</button>
 		</>
 	);
 };
