dd83cccefb418eaecd4b6e55a1c5a7ca8c0634f3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-19 00:19:34 -0500

    18. Parametrizar y pulir nuestro custom hook

--- src/bases/CounterHook.tsx
+++ src/bases/CounterHook.tsx
@@ -1,12 +1,14 @@
 import { useCounter } from '../hooks/useCounter';
 
 export const CounterHook = () => {
-	const { counter, counterElement, handleClick } = useCounter();
+	const { counter, elementToAnimate, handleClick } = useCounter({
+		maxCount: 15
+	});
 
 	return (
 		<>
 			<h1>Counter Hook:</h1>
-			<h2 ref={counterElement}>{counter}</h2>
+			<h2 ref={elementToAnimate}>{counter}</h2>
 			<button onClick={handleClick}>+1</button>
 		</>
 	);
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,40 +1,40 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
-const MAXIMUN_COUNT = 10;
-
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 10 }) => {
 	const [counter, setCounter] = useState(5);
-	const counterElement = useRef<HTMLHeadingElement>(null);
+	const elementToAnimate = useRef<any>(null);
+
+	const tl = useRef(gsap.timeline());
 
 	const handleClick = () => {
-		setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
+		setCounter((prev) => Math.min(prev + 1, maxCount));
 	};
 
+	useLayoutEffect(() => {
+		if (!elementToAnimate.current) return;
+
+		tl.current
+			.to(elementToAnimate.current, {
+				y: -10,
+				duration: 0.2,
+				ease: 'ease.out'
+			})
+			.to(elementToAnimate.current, {
+				y: 0,
+				duration: 1,
+				ease: 'bounce.out'
+			})
+			.pause();
+	}, []);
+
 	useEffect(() => {
-		if (counter < 10) return;
-
-		console.log(
-			'%cSe llego al valor máximo',
-			'color: red; background-color: black'
-		);
-
-		const tl = gsap.timeline();
-
-		tl.to(counterElement.current, {
-			y: -10,
-			duration: 0.2,
-			ease: 'ease.out'
-		}).to(counterElement.current, {
-			y: 0,
-			duration: 1,
-			ease: 'bounce.out'
-		});
+		tl.current.play(0);
 	}, [counter]);
 
 	return {
 		counter,
-		counterElement,
+		elementToAnimate,
 		handleClick
 	};
 };
